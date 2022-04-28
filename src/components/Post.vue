aa7e743101e1c878dd8f041009479e088304a9b3
AustinDye <thedyeguy1996@outlook.com>
2022-04-28 15:02:25 -0600

    posts

--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="row">
-    <div class="col-12 border-2">{{ post.body }}</div>
+    <div class="col-8 mx-5">
+      <div class="card shadow-sm">
+        <span class="d-flex mx-3 my-2">
+          <img :src="post.creator.picture" v-if="post.creator.picture" class="rounded-pill"/>
+          <h6 class="mx-4 pt-3">{{post.creator.name}}</h6>
+          </span>
+        <h5 class="text-center">{{post.body}}</h5>
+        <img :src="post.imgUrl" v-if="post.imgUrl" class="img-fluid"/>
+      </div>
+      <br>
+    </div>
   </div>
 </template>
 
@@ -21,4 +31,9 @@ export default {
 
 
 <style lang="scss" scoped>
+.rounded-pill{
+  max-height: 10%;
+  max-width: 10%;
+}
+
 </style>
